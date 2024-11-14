7ccb2c6c0dfce16195167300cfd3b0459e4b72ed
galeen <galeengames@gmail.com>
2024-11-14 12:51:21 +0100

    changes

---
+++ project-root/app/Controllers/Renderer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+use CodeIgniter\Exceptions\PageNotFoundException;
+
+class Renderer extends BaseController
+{
+    public static function show($page = "home", $extra_data = [])
+    {
+        if(!is_file(APPPATH . "Views/" . $page . ".php")){
+            throw new PageNotfoundException($page);
+        }
+
+        $meta = [
+            "title" => "PC Part Picker",
+            "cssfiles" => $extra_data["cssfiles"] ?? [],
+        ];
+        $context = $extra_data;
+        return view("templates/head", $meta)
+        . view($page, $context)
+        . view("templates/footer", $meta);
+    }
+}
